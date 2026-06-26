870acae8dbaa7cec0cc99a1821c0431f149839a1
Simon Wacker <simon.wacker@ise.faunhofer.de>
2026-06-26 19:24:39 +0200

    Add migration that adds `SOLAR_SCREEN` to the component subtype enumeration

--- backend/src/Migrations/migrate.sql
+++ backend/src/Migrations/migrate.sql
@@ -3469,6 +3469,7 @@ BEGIN
     VALUES ('20260611161712_AssertExistenceOfAccessPolicyAndRootResourceInPostgreSql', '10.0.8');
     END IF;
 END $EF$;
+
 COMMIT;
 
 START TRANSACTION;
@@ -3494,5 +3495,24 @@ BEGIN
     VALUES ('20260620114038_MakeCoatedSideOfOpticalDataNonNullable', '10.0.9');
     END IF;
 END $EF$;
+COMMIT;
+
+START TRANSACTION;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260626170958_AddSolarScreenToComponentSubtype') THEN
+    ALTER TYPE database.optical_component_subtype ADD VALUE 'solar_screen' AFTER 'shade_material';
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260626170958_AddSolarScreenToComponentSubtype') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20260626170958_AddSolarScreenToComponentSubtype', '10.0.9');
+    END IF;
+END $EF$;
+
 COMMIT;
 
